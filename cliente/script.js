b9461da100cdfcde7f15d2e4317dfe7d87ed94f6
Maryolivera <oliveramary33@gmail.com>
2025-04-14 19:53:05 -0300

    función jugar-función mostrarPregunta
    carga países y llama a mostrarPregunta, controla numero de preguntas y con un numero aleatorio elije una de las 3 opciones de pregunta

--- cliente/script.js
+++ cliente/script.js
@@ -1,4 +1,11 @@
 let listaPaises=[];
+let correctas = 0;
+let incorrectas = 0;
+let tiempoInicio;
+let numeroPregunta = 1;
+const totalPreguntas = 10;
+
+
 async function cargarPaises() {
     try {
       const respuesta = await fetch("https://restcountries.com/v3.1/all");
@@ -10,3 +17,45 @@ async function cargarPaises() {
       alert("No se pudieron cargar los datos de los países.");
     }
   }
+
+
+  async function jugar() {
+    document.getElementById("pantalla-inicio").style.display = "none";
+    document.getElementById("pantalla-resultados").style.display = "none";
+    document.getElementById("pantalla-juego").style.display = "flex";
+  
+    correctas = 0;
+    incorrectas = 0;
+    numeroPregunta = 1;
+    tiempoInicio = Date.now();
+  
+    await cargarPaises(); 
+  
+    mostrarPregunta();
+  } 
+
+
+
+  function mostrarPregunta() {
+    if (numeroPregunta > totalPreguntas) {
+      mostrarPantallaResultados();
+      return;
+    }
+  
+    const tipo = Math.floor(Math.random() * 3); 
+  
+    if (tipo === 0) {
+      mostrarPreguntaCapital();
+    } else if (tipo === 1) {
+      
+      mostrarPreguntaBandera(); 
+    } else {
+      
+      mostrarPreguntaLimitrofes(); 
+    }
+  }
+
+
+  
+
+  
