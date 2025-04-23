558f193e4195a71fdae5793b623ffa3c0b05cb5d
Maryolivera <oliveramary33@gmail.com>
2025-04-23 20:43:17 -0300

    agregar funcion para leer el rankig,,ordenarlo y enviarlo al cliente
    leer el archivo ranking ,generar 3 listas ordenadas por puntaje, aciertos y tiempo devolverlas para que el cliente las consuma

--- servidor/server.js
+++ servidor/server.js
@@ -3,8 +3,11 @@ const cors    = require('cors')
 const fs      = require('fs')
 
 const app = express()
+const PORT=3000
+
 app.use(cors())
 app.use(express.json())
+
 
 app.post('/guardarResultados', (req, res) => {
   const ranking = JSON.parse(fs.readFileSync('ranking.json', 'utf8'))
@@ -13,8 +16,40 @@ app.post('/guardarResultados', (req, res) => {
   res.json({ mensaje: 'Datos guardados', total: ranking.length })
 })
 
-app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'))
+app.get('/rankings', (req, res) => {
+  res.json(ordenarRanking())
+})
 
+function leerRanking() {
+  try {
+    return JSON.parse(fs.readFileSync('ranking.json', 'utf8'))
+  } catch {
+    return []
+  }
+}
+
+
+function ordenarRanking() {
+  const rankingCompleto = leerRanking()
+
+  const rankingPuntaje = [...rankingCompleto]
+    .sort((a, b) => b.puntajeTotal - a.puntajeTotal)
+    .slice(0, 20)
+
+  const rankingAciertos = [...rankingCompleto]
+    .sort((a, b) => b.correctas - a.correctas)
+    .slice(0, 20)
+
+  const rankingTiempo = [...rankingCompleto]
+    .sort((a, b) => a.duracionTotal - b.duracionTotal)
+    .slice(0, 20)
+
+  return { rankingPuntaje, rankingAciertos, rankingTiempo }
+}
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`)
+})
 
 
 
